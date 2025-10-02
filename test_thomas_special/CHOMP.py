ab2a883e86f73e1203c75844bfb557212e2d938e
Thomas <thomapau@stud.ntnu.no>
2025-10-02 11:38:57 +0200

    CHOMP is thriving

--- test_thomas_special/CHOMP.py
+++ test_thomas_special/CHOMP.py
@@ -2,27 +2,31 @@ import numpy as np
 from field import Field
 from path import Path
 import copy
-from Thomas_alg import Thomas
+from Thomas_alg import Thomas_spesial
 
 
 
 # -Δx_(i-1) + 2 * Δx_i - Δx_(i+1) = w_t * (w_s * (2*x_i - x_(i-1) - x_(i+1)) + w_o * g_i)
-def CHOMP(field: Field, start_path: Path, weight_obst: float, weight_smoothnes: float, weight_total: float) -> Path: # antar at start og slutt er start og slutt i start_path
+def CHOMP(field: Field, start_path: Path, weight_obst: float, weight_smoothnes: float, weight_total: float, safety_r = 6.0) -> Path: # antar at start og slutt er start og slutt i start_path
 
     path = copy.deepcopy(start_path)
     np_path = path.to_np_array()
 
     # Obstacle math:
         # g_i = nabla dist_punishment = (trough the infamus chain rule) = dist_punishment'(clearance) * nabla clearance(x) 
-    
-    # The punishment itselfe is as of now 1/2 * (clearance)**2 but this could change if i for example want a big safe distance OSLT
-    def change_in_dist_punishment_per_c(clearance):
-        return clearance
 
+    
     def obstacle_effect(point_xy):
-        dpunish_dclearance = change_in_dist_punishment_per_c(field.dist_to_closest(point_xy=point_xy)) 
-        obst_direction = field.dir_to_closest(point_xy=point_xy)
-        return [weight_obst * dpunish_dclearance * (-i) for i in obst_direction] # minus because it points toward the obstacle, but the gradient should be away
+        d = field.dist_to_closest(point_xy=point_xy)   # >=0 in free space
+        if d >= safety_r:
+            return np.array([0.0, 0.0])
+
+        # ∇d points AWAY from obstacle. If your dir_to_closest points TOWARD it,
+        # flip the sign:
+        grad_d = -np.array(field.dir_to_closest(point_xy=point_xy))  # unit, away
+
+        # -(r-d) * grad d is ∇c; we step along -∇c = +(r-d)*∇d
+        return weight_obst * (safety_r - d) * grad_d
 
     # Smoothnes math:
 
@@ -41,9 +45,9 @@ def CHOMP(field: Field, start_path: Path, weight_obst: float, weight_smoothnes: 
             obs_i[i] = 0
             continue
         
-        obs_i[i] += weight_smoothnes * (2*np_path[i] -  np_path[i-1] - np_path[i+1])
+        obs_i[i] += weight_smoothnes * (-2*np_path[i] +  np_path[i-1] + np_path[i+1])
         obs_i[i] *= weight_total
 
         
 
-    return Path(list(Thomas(np_path, obs_i)))
+    return Path(list(Thomas_spesial(np_path, obs_i)))
